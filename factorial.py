d016f59af4ed6c00aeee45941054939b7846f474
Bhavin Rajput <143064084+bhavik69600@users.noreply.github.com>
2025-08-21 18:20:01 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,4 +1,4 @@
-#31.	Write a program to find the factorial of a given number using recursion.
+# a program to find the factorial of a given number using recursion.
 
 def factorial(n):
     if n == 0 or n == 1:
@@ -13,4 +13,5 @@ number = int(input("Enter a number to find its factorial: "))
 result = factorial(number)
 # Display result
 print(f"The factorial of {number} is: {result}")
+
 
